8e849edc698321355d41707017b01730605ea6c5
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-15 02:29:37 +0500

    Initial commit

---
+++ ap.js
@@ -0,0 +1,66 @@
+
+// ----------logic-building-small-project-01(Month days teller)---------------------------
+// var monthName = prompt('Enter Month name like(january,february,march,april,may,june,july,august,september,october,november or december');
+// var january   = 30;
+// var february   = 28;
+// var march      = 31;
+// var april      = 30;
+// var may        = 31;
+// var  june      = 30;
+// var  july      = 30;
+// var  august    = 31;
+// var september  = 30;
+// var october    = 31;
+// var  november  = 30;
+// var december   = 31;
+// if (monthName === 'january'){console.log('The month of January has : ' + january + ' days ');}
+// else if (monthName === 'february') {console.log('The month of February has : ' + february + ' day ');}
+// else if (monthName === 'march') {console.log('The month of March has : ' + march + ' day ');}
+// else if (monthName === 'april') {console.log('The month of April has : ' + april + ' day ');}
+// else if (monthName === 'may') {console.log('The month of May has : ' + may + ' day ');}
+// else if (monthName === 'june') {console.log('The month of June has : ' + june + ' day ');}
+// else if (monthName === 'july') {console.log('The month of July has : ' + july + ' day ');}
+// else if (monthName === 'august') {console.log('The month of August has : ' + august + ' day ');}
+// else if (monthName === 'september') {console.log('The month of September has : ' + september + ' day ');}
+// else if (monthName === 'october') {console.log('The month of October has : ' + october + ' day ');}
+// else if (monthName === 'november') {console.log('The month of November has : ' + november + ' day ');}
+// else if (monthName === 'december') {console.log('The month of December has : ' + december + ' day ');}
+// else{console.log('you have entered wrong content');}
+
+
+//----------logic-building-small-project-02(guess number checker)---------------------------
+        // var user_Guess = prompt('Enter number that you guess');
+        // var actual_Number = '999';
+        // if (user_Guess === actual_Number) {console.log(' welldone! your guessed no is correct');}
+        // else{console.log('your guessed number is incorrect');}
+
+
+//----------logic-building-small-project-03(positive or negative number checker)---------------------------
+    // var enter_Number = prompt('Enter any number');
+    // if (enter_Number > 0) {console.log('Welldone! you entered a positive number');}
+    // else{console.log('Oh,ho! you entered a negative number');}
+
+
+//----------logic-building-small-project-04(password for login)---------------------------
+            // var enter_Password = prompt('Enter password for log in account');
+            // var password = '@67bc';
+            // if (enter_Password === password){console.log('Welcome! log in your account');}
+            // else{console.log('Sorry,try again, you have entered an incorrect password');}
+
+//----------logic-building-small-project-01(Month days teller)---------------------------
+    var enter_Phone_Number = prompt('Enter your phone number for log In');
+    var phone_Number = '0301-1234567';
+    if (enter_Phone_Number !== phone_Number){console.log('You have not already account plz Create an account and login again');}
+    else{console.log()
+    }
+//----------logic-building-small-project-01(Month days teller)---------------------------
+
+//----------logic-building-small-project-01(Month days teller)---------------------------
+
+//----------logic-building-small-project-01(Month days teller)---------------------------
+
+//----------logic-building-small-project-01(Month days teller)---------------------------
+
+//----------logic-building-small-project-01(Month days teller)---------------------------
+
+//----------logic-building-small-project-01(Month days teller)---------------------------
