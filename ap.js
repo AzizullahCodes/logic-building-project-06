cc2d90ab3763b2d6c0a18a1ef102bbb312ad385c
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-15 19:46:35 +0500

    u

--- ap.js
+++ ap.js
@@ -47,20 +47,114 @@
             // if (enter_Password === password){console.log('Welcome! log in your account');}
             // else{console.log('Sorry,try again, you have entered an incorrect password');}
 
-//----------logic-building-small-project-01(Month days teller)---------------------------
-    var enter_Phone_Number = prompt('Enter your phone number for log In');
-    var phone_Number = '0301-1234567';
-    if (enter_Phone_Number !== phone_Number){console.log('You have not already account plz Create an account and login again');}
-    else{console.log()
-    }
-//----------logic-building-small-project-01(Month days teller)---------------------------
+//----------logic-building-small-project-05(Login Verification '&& operator')---------------------------
+    // var userName = prompt('Enter user name');
+    // var password = prompt('Enter your login password');
+    // var name = 'aziz';
+    // var pass = '@765';
+    // if(userName === name && password === pass){console.log('Welcome to your account');}
+    // else{console.log('Yours login information is wrong');}
+    
+//----------logic-building-small-project-06(Login verification '|| operator')---------------------------
+        // var userName = prompt('Enter user name');
+        // var password = prompt('Enter your login password');
+        // var name = 'aziz';
+        // var pass = '@765';
+        // if(userName === name || password === pass){console.log('Welcome to your account');}
+        // else{console.log('Yours login information is wrong');}
+//----------logic-building-small-project-07(Number range checker))---------------------------
+    //  var number = prompt('Enter  a number');
+    //  if(number >= 10 && number <= 20){console.log('your number is inside range');}
+    //  else{console.log('you number is out of range');}
+     
+//----------logic-building-small-project-08(Discount Eligibility)---------------------------
+        // var condition_1 = prompt('Enter the profession of user(student or else)');
+        // var condition_2 = prompt('Enter the age of user'); 
+        // if(condition_1 === 'student' && condition_2 > 60){console.log('You are eligible for discount');}
+        // else{console.log('You are not eligible for discount');}
+//----------logic-building-small-project-09(pass or fail result checker)---------------------------
+// var enterEnglishMarks = +prompt('Enter your obtained marks in English');
+// var enterMathMarks = +prompt('Enter your obtained marks in Math');
+// var enterUrduMarks = +prompt('Enter your obtained marks in Urdu');
+// var enterScienceMarks = +prompt('Enter your obtained marks in Science');
+// var total_Marks = 400;
+// var passing_Marks= 310;
+// var obtained_Marks = enterEnglishMarks + enterMathMarks + enterScienceMarks + enterUrduMarks;
+// if (obtained_Marks >= passing_Marks){console.log('Congratulation, you got ' + obtained_Marks + ' Marks and you have passed');}
+// else{console.log('Sorry,you have failed');}
+//----------logic-building-small-project-10(Room Entry Checker)---------------------------
+//  var ticket = prompt('Do you have ticket (answer yes or no)');
+//  var age    = prompt('Enter your age in number like "10,14,34,etc"');
+//  if (ticket === 'yes' && age > 20){console.log('You can enter');}
+//  else{console.log('Your can not enter');}
 
-//----------logic-building-small-project-01(Month days teller)---------------------------
+//----------logic-building-small-project-11(Time of Day Greeter)---------------------------
+        // var timeOfEntery = prompt('Enter time of entry like (morning,after-noon,evening,night)');
+        // if (timeOfEntery === 'morning'){console.log('Good-morning');}
+        // else if (timeOfEntery === 'after-noon'){console.log('Good-afternoon');}
+        // else if (timeOfEntery === 'evening'){console.log('Good-evening');}
+        // else if (timeOfEntery === 'night'){console.log('Good-night');}
+        // else{console.log('Enter suitable input');}
 
-//----------logic-building-small-project-01(Month days teller)---------------------------
+//----------logic-building-small-project-12(Show-warning if battery low)---------------------------
+        // var enterBatteryPercentage = prompt('Enter mobile Battery pecentage like(30,60,90,100 etc)');
+        // if (enterBatteryPercentage === 100) {console.log('you mobile battery has charged and has 100% charge,unplug');}
+        // else if (enterBatteryPercentage >= 80) {console.log('Your battery has good charging');}
+        // else if (enterBatteryPercentage >=50) {console.log('Your battery has enough charging');}
+        // else if (enterBatteryPercentage >= 30){console.log('your battery charging is low');}
+        // else if (enterBatteryPercentage <= 29 ) {console.log('plz charge your mobile');}
+        // else {console.log('plz enter proper input');}
 
-//----------logic-building-small-project-01(Month days teller)---------------------------
+//----------logic-building-small-project-13(Movie Access)---------------------------
+    //    var enter_age = prompt('Enter your age like (4,29,50)');
+    //    var parental_permission = prompt('Enter parental permission yes or no');
+    //    if (enter_age >= 13 || parental_permission === 'yes' ){console.log('You can watch movie');}
+    //    else {console.log('Your can not watch movie');}
 
-//----------logic-building-small-project-01(Month days teller)---------------------------
+//----------logic-building-small-project-14(positive or negative number checker)---------------------------
+//     var enterNumber = +prompt('Enter any number');
+//     if (enterNumber > 0){console.log('The number you entered is positive');}
+//    else if (enterNumber < 0){console.log('The number is negative');}
+//    else if (enterNumber === 0) {console.log('The number is zero');}
+// else{console.log('Enter proper input');}  
 
-//----------logic-building-small-project-01(Month days teller)---------------------------
+//----------logic-building-small-project-15(age checker)---------------------------
+        // var enterAge = prompt('Enter age');
+        // if (enterAge <= 10) {console.log('You are child');}
+        // else if (enterAge >= 11 && enterAge <= 17) {console.log('You are teen ager');}
+        // else if(enterAge >= 18) {console.log('you are adult');}
+        // else{console.log('Enter proper input');}
+    
+
+    //----------logic-building-small-project-16(Grade teller)---------------------------
+// var enterMathMarks = +prompt('Enter math obtainded marks out of 100');
+// var enterEnglishMarks = +prompt('Enter English obtainded marks out of 100');
+// var enterScienceMarks = +prompt('Enter Science obtainded marks out of 100');
+// var totalMarks = 300;
+// var totalObtainedMarks = enterEnglishMarks + enterMathMarks + enterScienceMarks;
+// var percentage = (totalObtainedMarks / totalMarks) * 100;
+// if (percentage >= 90 && percentage <= 100){console.log('You got A+ grade.');}
+// else if (percentage >= 80 && percentage < 90){console.log('You got A grade');}
+// else if (percentage >= 60 && percentage < 80) {console.log('You got B grade');}
+// else if (percentage >= 50 && percentage < 60) {console.log('You got C grade');}
+//  else if (percentage >= 33 && percentage < 50) {console.log('you got D grade');}
+//  else if (percentage < 33){console.log('I think you are the most idle student of class and you are failed');}
+//  else {console.log('Enter proper content');}
+
+    //----------logic-building-small-project-11(pray if money is low or high)---------------------------
+    // var money = prompt('Enter money that you want to begger between 1 to 100');
+    // if (money <= 5){console.log('Begger prays for the failing of him in examination');}
+    // else if (money >= 6 && money <= 50) {console.log('Begger prays for the passing of him');}
+    // else if (money > 50) {console.log('Begger  prays for good marks of him');}
+    // else{console.log('Enter suitable content');}
+    //----------logic-building-small-project-11(Month days teller)---------------------------
+
+    //----------logic-building-small-project-11(Month days teller)---------------------------
+
+    //----------logic-building-small-project-11(Month days teller)---------------------------
+
+    //----------logic-building-small-project-11(Month days teller)---------------------------
+
+    //----------logic-building-small-project-11(Month days teller)---------------------------
+
+    //----------logic-building-small-project-11(Month days teller)---------------------------
